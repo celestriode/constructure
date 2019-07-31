e0d47044fd1434709aa3ac2971c8f48395f2ecf5
Celestriode <celestriode@gmail.com>
2019-07-31 16:31:08 -0400

    Extra prettifier methods

--- src/Reports/PrettifySupplierInterface.php
+++ src/Reports/PrettifySupplierInterface.php
@@ -14,4 +14,29 @@ interface PrettifySupplierInterface
      * @return string
      */
     public function prettify(string $string): string;
+
+    /**
+     * Prettifies a string that is designated as a value.
+     *
+     * @param string $value The string to prettify.
+     * @return string
+     */
+    public function prettifyValue(string $value): string;
+
+    /**
+     * Prettifies a string that is designated as a key.
+     *
+     * @param string $key The string to prettify.
+     * @return string
+     */
+    public function prettifyKey(string $key): string;
+
+    /**
+     * Prettifies a string using a supplied closure.
+     *
+     * @param string $string The string to prettify using the closure.
+     * @param \Closure $func The closure to prettify the string with.
+     * @return string
+     */
+    public function prettifyDynamic(string $string, \Closure $func): string;
 }
